41a5790b30f7169653d0a694a85e63c465588883
Eiman Males <maleseiman@gmail.com>
2022-09-20 23:05:12 -0500

    Agregar archivos de proyecto.

---
+++ PROYECTO_ROL6/Controllers/AccesoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using PROYECTO_ROL6.Models;
+using PROYECTO_ROL6.Data;
+
+namespace PROYECTO_ROL6.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index(Usuario _usuario)
+        {
+            DA_Logica _da_logica = new DA_Logica();
+
+            string nombre = _da_logica.validarUsuario(_usuario.Correo, _usuario.Clave);
+            return View();
+        }
+    }
+}
---
+++ PROYECTO_ROL6/Data/DA_Logica.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using PROYECTO_ROL6.Models;
+
+namespace PROYECTO_ROL6.Data
+{
+    public class DA_Logica
+    {
+        public List<Usuario> listaUsuarios()
+        {
+            return new List<Usuario>
+            {
+                new Usuario{Nombre = "Eiman", Correo = "eiman@correo.com", Clave = "123", Roles = new string[]{"Administrador"} }
+            };
+        }
+
+        public Usuario validarUsuario(string _correo, string _clave)
+        {
+            return listaUsuarios().Where(_correo == )
+        }
+    }
+}
